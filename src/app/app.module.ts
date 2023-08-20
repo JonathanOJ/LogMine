19e23bce4cf198d57859b94eb1da35fde8201d16
Jonathan Jacobovski <jonathan.ojacobovski@gmail.com>
2023-08-19 21:00:32 -0300

    adicionado lib para icones e finalizado o footer e a versao mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { BrowserModule, DomSanitizer } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LogmineHeaderComponent } from "./logmine-header/logmine-header.component";
 import { LogmineBodyComponent } from "./logmine-body/logmine-body.component";
 import { LogmineFooterComponent } from "./logmine-footer/logmine-footer.component";
-import { LogmineButtonComponent } from './logmine-button/logmine-button.component';
+import { LogmineButtonComponent } from "./logmine-button/logmine-button.component";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-	declarations: [AppComponent, LogmineHeaderComponent, LogmineBodyComponent, LogmineFooterComponent, LogmineButtonComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [
+		AppComponent,
+		LogmineHeaderComponent,
+		LogmineBodyComponent,
+		LogmineFooterComponent,
+		LogmineButtonComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule, MatIconModule, HttpClientModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+	constructor(private sanitizer: DomSanitizer, private matIconRegistry: MatIconRegistry) {
+		matIconRegistry.addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl("./assets/mdi.svg"));
+	}
+}
